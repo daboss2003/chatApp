2ee53f89bafcc8a6d766ec298841fcfa5f4c7714
daboss2003 <samsonoluwafemi@gmail.com>
2024-04-22 13:14:48 +0100

    removed api_key

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB2atMdt1pklaKHBVdpccfoSZVslCETL6k",
-  authDomain: "chatapp-db474.firebaseapp.com",
-  projectId: "chatapp-db474",
-  storageBucket: "chatapp-db474.appspot.com",
-  messagingSenderId: "197904212023",
-  appId: "1:197904212023:web:deac0c4fc10f376b321fb2",
-  measurementId: "G-XDYBGBKK7H"
+  apiKey: process.env.REACT_APP_APP_ID,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_KEY,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
